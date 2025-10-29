eb462a673ebf1e55146f1eb2d653aadba5bc29a7
Harkanwal Singh Ji <harkanwal.singh@indiamart.com>
2025-10-29 19:18:14 +0530

    Created Group Routes in nextJS app

--- app/(users)/layout.js
+++ app/(users)/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Nav from "../component/nav";
+import "../globals.css";
+import Nav from "../../component/nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
---
+++ app/(users)/page.js
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-col items-center justify-center">
+        <h1>Users Home Page</h1>
+        <p>Welcome to the users section of our Next.js application.</p>
+      </main>
+    </div>
+  );
+}
