147ed15daeb5c2f3d3efd86bdf205652feaed647
Harkanwal Singh Ji <harkanwal.singh@indiamart.com>
2025-10-30 10:02:25 +0530

    Improved Nav UI

--- component/nav.jsx
+++ component/nav.jsx
@@ -2,12 +2,12 @@ import Link from 'next/link'
  
 export default function Nav() {
   return (
-        <nav className="flex gap-5 p-10">
+        <nav className="flex p-10 justify-center align-middle">
           {/* Prefetched when the link is hovered or enters the viewport */}
-          <Link href="/">Home</Link>
-          <Link href="/about">About us</Link>
-          <Link href="/service">Services</Link>
-          <Link href="/contact">Contact us</Link>
+          <Link href="/" className='p-2.5'>Home</Link>
+          <Link href="/about" className='p-2.5'>About us</Link>
+          <Link href="/service" className='p-2.5'>Services</Link>
+          <Link href="/contact" className='p-2.5'>Contact us</Link>
         </nav>
   )
 }
